7ec650227f9c06b18a35e937468d017c514bc94a
Oussama El Mabrouki <138411649+OussamaElm0@users.noreply.github.com>
2023-10-07 21:44:09 +0100

    Form finished

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,15 +3,16 @@ import React, {useState} from 'react';
 export default function Input({handleClick}) {
     const [inputValue, setInputValue] = useState("");
 
-    const handleChange = (e) => {
-        const {target: {value}} = e
-        setInputValue(value)
-    }
-
     return (
       <>
         <form>
-          <input type="text" id="todo-text" name="todo" value={inputValue} onChange={(e) => {handleChange(e)}}/>
+          <input 
+            type="text" 
+            id="todo-text" 
+            name="todo" 
+            value={inputValue} 
+            onChange={({target: {value}}) => setInputValue(value) }
+          />
           <input 
             type='submit' 
             value="Add" 
